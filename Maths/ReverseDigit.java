abdc71aea4b786750c35ffa10333bf5cdb4f5a75
Sahil Ghadigaonkar <83874177+sahilghadi47@users.noreply.github.com>
2024-04-03 18:02:05 +0530

    Update ReverseDigit.java

--- Maths/ReverseDigit.java
+++ Maths/ReverseDigit.java
@@ -2,22 +2,21 @@ package Maths;
 
 public class ReverseDigit {
     static int sol(int n) {
+        
         int ans = 0;
 
-        while (n/10> 0) {
-
-            int dig = n % 10;
-            if (ans > Integer.MAX_VALUE / 10 || ans < Integer.MIN_VALUE / 10)
-                return 0;
-            ans = dig + ans * 10;
-            n /= 10;
-
+        while( n!=0){
+            int dig = n%10;
+            n/=10;
+            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && dig > 7)) return 0;
+            if (ans < Integer.MIN_VALUE / 10 || (ans == Integer.MIN_VALUE / 10 && dig < -8)) return 0;
+            ans = ans*10+ dig;
         }
         return ans;
     }
 
     public static void main(String[] args) {
-        int n = 1234056789;
+        int n = 123409;
         System.out.println(sol(n));
     }
 }
