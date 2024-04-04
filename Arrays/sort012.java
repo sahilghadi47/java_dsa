4988db674c5bf4c2fc4dc3592fa2270d63e38733
Sahil Ghadigaonkar <83874177+sahilghadi47@users.noreply.github.com>
2024-04-04 20:18:26 +0530

    Create sort012.java

---
+++ Arrays/sort012.java
@@ -0,0 +1,59 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class sort012 {
+    static void brute(int arr[]){
+        // Arrays.sort(arr);
+        
+
+        int n= arr.length;
+        int zero=0;
+        int one=0;
+        int two=0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==0) zero++;
+            if(arr[i]==1) one++;
+            if(arr[i]==2) two++;
+        }
+        int i ;
+        for(i=0;i<zero;i++){
+            arr[i]=0;
+        }
+        for(i=zero;i<zero+one;i++){
+            arr[i]=1;
+        }
+        for(i=zero+one;i<n;i++){
+            arr[i]=2;
+        }
+    }
+
+    static void optimised(int arr[]){
+        //using 3 pointers
+        //i.e low =cheak 0;
+        //mid = cheak 1;
+        //high = cheak 2;
+        
+        int low=0;
+        int mid =0;
+        int high = arr.length -1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                swap.sol(arr,low , mid);
+                low++;
+                mid++;
+            }else if(arr[mid]==1){
+                mid++;
+            }
+            else{
+                swap.sol(arr,mid ,high);
+                high--;
+            }
+        } 
+    }
+    public static void main(String[] args) {
+        int arr[] = {2,1,2,2,0,0,1,1};
+        optimised(arr);
+        printArray.print(arr);
+    }
+}
