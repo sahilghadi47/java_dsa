5ab515351744db333a01572005fad5a9a57cabf2
Sahil Ghadigaonkar <83874177+sahilghadi47@users.noreply.github.com>
2024-04-07 19:29:22 +0530

    Update bubbleSort.java

--- sortingAlgos/bubbleSort.java
+++ sortingAlgos/bubbleSort.java
@@ -10,7 +10,7 @@ public class bubbleSort {
         //ITERATIVE FUNCTION
         int size = arr.length-1;
         for(int i = 0;i<size;i++){
-            for(int j =0;j<size-1;j++){
+            for(int j =0;j<size-i;j++){
                 if(arr[j]>arr[j+1])
                     swap(arr, j);
             }
