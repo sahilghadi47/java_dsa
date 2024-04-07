b75819481ae41a2586047e4715e3b9a331ec2d0a
Sahil Ghadigaonkar <83874177+sahilghadi47@users.noreply.github.com>
2024-04-07 19:07:16 +0530

    Update bubbleSort.java

--- sortingAlgos/bubbleSort.java
+++ sortingAlgos/bubbleSort.java
@@ -1,20 +1,34 @@
 package sortingAlgos;
 
 public class bubbleSort {
-    static void sort(int arr[] , int n){
-        for(int i =0;i<n;i++){
-            for(int j=1;j<n-i ;j++){
-                if(arr[j-1]>arr[j]){
-                    swap.arraysort(arr, j-1, j);
-                }
+    void swap(int[] arr, int j) {
+        int temp =arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1]=temp;
+    }
+    void sort(int arr[] ){
+        //ITERATIVE FUNCTION
+        int size = arr.length-1;
+        for(int i = 0;i<size;i++){
+            for(int j =0;j<size-1;j++){
+                if(arr[j]>arr[j+1])
+                    swap(arr, j);
             }
         }
-        
+    }
+    void sort(int arr[] ,int size){
+        //RECURSIVE FUNCTION
+        if(size ==0) return; 
+        for(int j =0;j<size-1;j++){
+            if(arr[j]>arr[j+1])
+                swap(arr, j);
+        }
+        sort(arr, size-1);
     }
     public static void main(String[] args) {
-        int arr[]= {2,5,6,5,3,1,0,1,4};
-        sort(arr, arr.length);
-        print.printArray(arr);
-    
+        int arr[] = {3,5,2,4,6,2,1,4,7,5};
+        BubbleSort ans = new BubbleSort();
+        ans.sort(arr, arr.length);
+        printArray.sol(arr);
     }
 }
